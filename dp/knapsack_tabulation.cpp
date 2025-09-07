c23cabfae17871bc95d0fc475823ac5e3e2098c2
desireUjjwal12 <ineffableujjwal@gmail.com>
2025-09-07 10:55:13 +0530

    Create knapsack_tabulation.cpp

---
+++ dp/knapsack_tabulation.cpp
@@ -0,0 +1,20 @@
+# Gfg problem link --> https://www.geeksforgeeks.org/problems/0-1-knapsack-problem0945/1
+
+class Solution {
+  public:
+    int knapsack(int W, vector<int> &val, vector<int> &wt) {
+        int n = wt.size();
+        vector<vector<int>> dp(n+1, vector<int>(W+1, 0));
+        for(int i=1; i<n+1; i++){
+            for(int j=1; j<W+1; j++){
+                if(wt[i-1] <= j){
+                    dp[i][j] = max(val[i-1] + dp[i-1][j-wt[i-1]], dp[i-1][j]);
+                }else{
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+        return dp[n][W];
+        
+    }
+};
